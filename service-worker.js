551fd62427b0bcc625be9b3fc470f09b65d002b9
Jereme <michael@jeremehancock.com>
2024-12-02 13:04:35 -0600

    updated to a PWA

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  'index.html',
+  'images/icons/web-app-manifest-192x192.png', 
+  'images/icons/web-app-manifest-512x512.png'
+];
+
+// Install the service worker and cache resources
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Serve cached resources when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
