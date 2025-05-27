6d54bc1228136cc6fa4e65bfb6c669610d811de6
Jereme <michael@jeremehancock.com>
2025-05-27 13:29:26 -0500

    added space shooter 2'

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,33 @@
-const CACHE_NAME = 'pwa-cache-v5';
+const CACHE_NAME = "pwa-cache-v5.1";
 const urlsToCache = [
-  'index.html',
-  'games/nerdle/index.html',
-  'games/pipes/index.html',
-  'games/memory/index.html',
-  'games/minesweeper/index.html',
-  'games/snake/index.html',
-  'games/soccer-juggle/index.html',
-  'games/water-ring-toss/index.html',
-  'games/waveform/index.html',
-  'games/bubble-pop/index.html',
-  'games/breakout/index.html',
-  'games/space-shooter/index.html',
-  'images/icons/web-app-manifest-192x192.png', 
-  'images/icons/web-app-manifest-512x512.png'
+  "index.html",
+  "games/nerdle/index.html",
+  "games/pipes/index.html",
+  "games/memory/index.html",
+  "games/minesweeper/index.html",
+  "games/snake/index.html",
+  "games/soccer-juggle/index.html",
+  "games/water-ring-toss/index.html",
+  "games/waveform/index.html",
+  "games/bubble-pop/index.html",
+  "games/breakout/index.html",
+  "games/space-shooter/index.html",
+  "images/icons/web-app-manifest-192x192.png",
+  "images/icons/web-app-manifest-512x512.png",
 ];
 
 // Install the service worker and cache resources
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
 // Serve cached resources when offline
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request))
   );
 });
-
