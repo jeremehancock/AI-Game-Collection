c6a4fbeee5411a783e7504d36af54531fb0b0d84
Jereme Hancock <1692414+jeremehancock@users.noreply.github.com>
2025-10-15 14:03:40 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,6 @@
-const CACHE_NAME = "pwa-cache-v9.8";
+const CACHE_NAME = "pwa-cache-v9.9";
 const urlsToCache = [
+  "/",
   "index.html",
   "games/nerdle/index.html",
   "games/pipes/index.html",
@@ -26,15 +27,32 @@ const urlsToCache = [
 // Install the service worker and cache resources
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activate and clean up old caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
 // Serve cached resources when offline
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches
-      .match(event.request)
+    caches.match(event.request)
       .then((response) => response || fetch(event.request))
   );
 });
